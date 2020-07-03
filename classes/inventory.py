c115f969400290b51c8a4f5fe6ce1f0ccc4a4e91
Phạm Trọng Nhân <tendaigom@gmail.com>
2020-07-03 16:32:53 +0700

    Finish game

---
+++ classes/inventory.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name = "Potion", type = "potion", description = "", prop = 50):
+        self.name = name
+        self.type = type
+        self.description = description
+        self.prop = prop
